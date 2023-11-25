9a4ddc4e3c162adcfdb38325aa9f359ef07a6fcc
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-11-25 09:14:42 +0500

    some changed

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,6 +1,12 @@
 const AboutPage = () => {
-  return (
-    <h1>ABOUT PAGE</h1>
-  )
-}
-export default AboutPage
+	return (
+		<>
+			<h1>ABOUT PAGE</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit necessitatibus inventore repellat accusantium beatae, pariatur veritatis molestias
+				illum, saepe, delectus eaque ex! Molestiae fuga voluptates molestias laudantium autem? Quasi, sit?
+			</p>
+		</>
+	);
+};
+export default AboutPage;
