be83c47fc711a199755d6ce25703eae2c8030989
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-11-27 08:15:12 +0500

    update all styles

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,4 +1,4 @@
-import { useThemeToggle } from '../../../app/providers/ThemeProvider';
+import { useThemeToggle } from 'src/app/providers/ThemeProvider';
 
 const MainPage = () => {
 	const toggleTheme = useThemeToggle();
@@ -7,6 +7,11 @@ const MainPage = () => {
 		<>
 			<p className='app'>Lira Earrings</p>
 			<h1>Something text</h1>
+			<h2>Something text</h2>
+			<h3>Something text</h3>
+			<h4>Something text</h4>
+			<h5>Something text</h5>
+			<div className='square'></div>
 			<button onClick={toggleTheme}>Переключить тему</button>
 		</>
 	);
