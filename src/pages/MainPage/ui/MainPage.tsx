ed40cca6d9b883df7e92d713cdccd814365f43e0
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-11-28 07:22:38 +0500

    translate

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,14 @@
+import { useTranslation } from 'react-i18next';
 import { useThemeToggle } from 'src/app/providers/ThemeProvider';
 
 const MainPage = () => {
 	const toggleTheme = useThemeToggle();
+	const { t, i18n } = useTranslation('main');
 
 	return (
 		<>
 			<p className='app'>Lira Earrings</p>
-			<h1>Something text</h1>
+			<h1>{t('h1')}</h1>
 			<h2>Something text</h2>
 			<h3>Something text</h3>
 			<h4>Something text</h4>
