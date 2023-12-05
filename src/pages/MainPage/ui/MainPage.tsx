e7b05b19b3c80932951924b1f347eddf9017e721
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-12-05 06:38:21 +0500

    modal settings

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,13 +1,25 @@
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Button, ThemeButton } from 'src/shared/ui/Button';
 import Modal from 'src/shared/ui/Modal/ui/Modal';
 
 const MainPage = () => {
   const { t } = useTranslation('translation', { keyPrefix: 'mainPage' });
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div data-testid="main-page">
       <h1>{t('title')}</h1>
-      <Modal />
+      <Button
+        theme={ThemeButton.FILLED}
+        onClick={() => setIsOpen(true)}
+      >
+        Modal toggle
+      </Button>
+      <Modal
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      />
     </div>
   );
 };
--- src/shared/ui/Modal/ui/Modal.tsx
+++ src/shared/ui/Modal/ui/Modal.tsx
@@ -5,15 +5,35 @@ import { classNames } from 'src/shared/lib/classNames/classNames';
 interface ModalProps {
   className?: string;
   children?: ReactNode;
+  isOpen?: boolean;
+  onClose?: () => void;
 }
 
-const Modal = ({ className, children }: ModalProps) => (
-  <div className={classNames(styles.root, {}, [className])}>
-    <div className={styles.overlay}>
-      <div className={styles.content}>{children}</div>
+const Modal = ({ className, children, onClose, isOpen }: ModalProps) => {
+  const mods: Record<string, boolean> = {
+    [styles.opened]: isOpen,
+  };
+
+  const closeHandler = () => {
+    if (onClose) {
+      onClose();
+    }
+  };
+
+  const onContentClick = (e: React.MouseEvent) => {
+    e.stopPropagation(); 
+  }
+
+  return (
+    <div className={classNames(styles.root, mods, [className])}>
+      <div
+        className={styles.overlay}
+        onClick={closeHandler}
+      >
+        <div className={styles.content} onClick={onContentClick}>{children}</div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Modal;
- 
